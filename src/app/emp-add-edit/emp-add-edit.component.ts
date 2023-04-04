59963bc707323d1147367331650b6a3e3dbb80cf
augusto-pacheco <augsutop407@outlook.com>
2023-04-03 23:16:47 -0600

    peticion get funcional, aun existen errores

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Task } from '../models/task.model';
-import { TasksService } from '../services/api-service.service';
 
 @Component({
   selector: 'app-emp-add-edit',
   templateUrl: './emp-add-edit.component.html',
   styleUrls: ['./emp-add-edit.component.scss']
 })
-export class EmpAddEditComponent implements OnInit {
+export class EmpAddEditComponent {
   empForm: FormGroup;
-  tasks: Task[] = [];
 
   test: string[] = [
     'texto 1',
@@ -31,7 +28,7 @@ export class EmpAddEditComponent implements OnInit {
     'texto 4'
   ]
 
-  constructor(private _fb: FormBuilder, private _tasksService: TasksService) {
+  constructor(private _fb: FormBuilder) {
     this.empForm = this._fb.group({
       nombre: '',
       registro: '',
@@ -41,13 +38,6 @@ export class EmpAddEditComponent implements OnInit {
       proyecto: '',
       tipo: '',
       categoria: ''
-    });
-  }
-
-  ngOnInit() {
-    this._tasksService.getAllTasks().subscribe(tasks => {
-      this.tasks = tasks;
-      console.log(tasks); // para verificar que se recibieron las tareas
     });
   }
 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Task } from '../models/task.model';
+import { TasksService } from '../services/api-service.service';
+
+import { EmpAddEditComponent } from '../emp-add-edit/emp-add-edit.component';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
 
+  tasks: Task[] = [];
+
+  title = 'masterApp';
+
+  constructor(private _dialog: MatDialog, private _tasksService: TasksService) {}
+
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent);
+  }
+
+  ngOnInit() {
+    this._tasksService.getAllTasks().subscribe(tasks => {
+      this.tasks = tasks;
+      console.log(tasks); // para verificar que se recibieron las tareas
+    });
+  }
 }
